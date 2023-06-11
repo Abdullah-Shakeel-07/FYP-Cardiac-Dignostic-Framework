e9b6a6a6ccf4680e182eb3a8e7c4f340424e17a3
ali.raza@upstartcommerce.com <68740486+AliUrden@users.noreply.github.com>
2023-06-11 20:39:23 +0500

    Containerizing app

--- frontend/app.py
+++ frontend/app.py
@@ -87,4 +87,4 @@ def upload():
     return render_template('results.html', output=EF, slices = no_image, shapes = shape)
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run('0.0.0.0',debug=False)
